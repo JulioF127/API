2e9885fcb777c51f606268001549864d11e1cdff
Julio Fuentes <juliofuentes12@hotmail.com>
2022-09-02 22:08:04 -0600

    Correccion

--- models/nosql/tracks.js
+++ models/nosql/tracks.js
@@ -45,6 +45,51 @@ const TracksScheme = new mongoose.Schema(
         versionKey: false
     }
 );
+TracksScheme.statics.findAllData = function (){
+    const joinData = this.aggregate([
+        {
+            $lookup:{
+                from:"storages",
+                localField: "mediaId",
+                foreignField: "_id",
+                as: "audio",
+            },
+        }, 
+        {
+            $unwind: "$audio"
+        }
 
+    ])
+    return joinData
+};
+
+TracksScheme.statics.findOneData = function (id){
+    const joinData = this.aggregate([
+        {
+
+        $match:{
+            _id: mongoose.Types.ObjectId(id),
+        },
+    },
+    {
+
+            $lookup: {
+                from:"storages",
+                localField: "mediaId",
+                foreignField: "_id",
+                as: "audio",
+            },
+        },
+        {
+            $unwind: "$audio",
+        }
+
+    ]);
+    return joinData
+};
+
+
+TracksScheme.plugin(mongooseDelete, {overrideMethods:"all"});
+module.exports = mongoose.model("tracks", TracksScheme);
 TracksScheme.plugin(mongooseDelete, {overrideMethods:"all"});
 module.exports = mongoose.model("tracks", TracksScheme);
